90c9e319619f6457201fc8d05fee2743090aa10d
Mike Fluff <mf@mikefluff.com>
2009-09-17 15:17:35 +0400

    Spin added

--- Classes/PickerViewController.h
+++ Classes/PickerViewController.h
@@ -1,25 +1,75 @@
 //
-//  OverlayViewController.h
-//  TableView
+//  SpinViewController.h
+//  Spin
 //
-//  Created by iPhone SDK Articles on 1/17/09.
-//  Copyright www.iPhoneSDKArticles.com 2009. 
+//  Created by Jeff LaMarche on 1/28/09.
+//  Copyright Jeff LaMarche Consulting 2009. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@class RootViewController;
+#define kRowMultiplier				100
+#define kAccelerationThreshold		2.2
+#define kUpdateInterval				(1.0f/10.0f)
 
-@interface PickerViewController : UIViewController <UIPickerViewDelegate> {
-
-	RootViewController *rvController;
-	IBOutlet UIPickerView *picker;
-	NSMutableArray *arrayNo;
-	NSMutableArray *code;
-	NSMutableString *secret;
+@interface PickerViewController : UIViewController <UIPickerViewDelegate, UIPickerViewDataSource, UIAccelerometerDelegate>
+{
+	UIPickerView	*picker;
+	UIButton		*spinButton;
+	
+	NSArray			*component1Data;
+	NSArray			*component2Data;
+	NSArray			*component3Data;
+	NSArray			*component4Data;
+	NSArray			*component5Data;
+	NSArray			*component6Data;
+	
+	NSArray			*component1Labels;
+	NSArray			*component2Labels;
+	NSArray			*component3Labels;
+	NSArray			*component4Labels;
+	NSArray			*component5Labels;
+	NSArray			*component6Labels;
+	
+	NSArray			*component1BlurredLabels;
+	NSArray			*component2BlurredLabels;
+	NSArray			*component3BlurredLabels;
+	NSArray			*component4BlurredLabels;
+	NSArray			*component5BlurredLabels;
+	NSArray			*component6BlurredLabels;
+	
+@private
+	BOOL			isSpinning;
+	NSUInteger		spin1;
+	NSUInteger		spin2;	
+	NSUInteger		spin3;
+	NSUInteger		spin4;
+	NSUInteger		spin5;
+	NSUInteger		spin6;
 	
 }
-
-@property (nonatomic, retain) RootViewController *rvController;
-
+@property (nonatomic, retain) IBOutlet UIPickerView *picker;
+@property (nonatomic, retain) IBOutlet UIButton *spinButton;
+@property (nonatomic, retain) NSArray *component1Data;
+@property (nonatomic, retain) NSArray *component2Data;
+@property (nonatomic, retain) NSArray *component3Data;
+@property (nonatomic, retain) NSArray *component4Data;
+@property (nonatomic, retain) NSArray *component5Data;
+@property (nonatomic, retain) NSArray *component6Data;
+@property (nonatomic, retain) NSArray *component1Labels;
+@property (nonatomic, retain) NSArray *component2Labels;
+@property (nonatomic, retain) NSArray *component3Labels;
+@property (nonatomic, retain) NSArray *component4Labels;
+@property (nonatomic, retain) NSArray *component5Labels;
+@property (nonatomic, retain) NSArray *component6Labels;
+@property (nonatomic, retain) NSArray *component1BlurredLabels;
+@property (nonatomic, retain) NSArray *component2BlurredLabels;
+@property (nonatomic, retain) NSArray *component3BlurredLabels;
+@property (nonatomic, retain) NSArray *component4BlurredLabels;
+@property (nonatomic, retain) NSArray *component5BlurredLabels;
+@property (nonatomic, retain) NSArray *component6BlurredLabels;
+- (IBAction)spin;
+- (NSArray *)blurredLabelArrayFromLabelArray:(NSArray *)labelArray;
+- (NSArray *)labelArrayFromStringArray:(NSArray *)stringArray;
 @end
+
--- Classes/PickerViewController.m
+++ Classes/PickerViewController.m
@@ -1,131 +1,237 @@
 //
-//  OverlayViewController.m
-//  TableView
+//  SpinViewController.m
+//  Spin
 //
-//  Created by iPhone SDK Articles on 1/17/09.
-//  Copyright www.iPhoneSDKArticles.com 2009. 
+//  Created by Jeff LaMarche on 1/28/09.
+//  Copyright Jeff LaMarche Consulting 2009. All rights reserved.
 //
 
 #import "PickerViewController.h"
-#import "RootViewController.h"
-#define EXTRA_COMPONENTS (10)
+#import "UIView-GetImageOf.h"
+#import "UIImage-Blur.h"
+
+@interface UIPickerView(SoundsEnabledPrivate)
+- (void)setSoundsEnabled:(BOOL)isEnabled;
+@end
+
+@interface UIImage(ResizePrivate)
+- (id)_imageScaledToSize:(struct CGSize)size interpolationQuality:(int)quality;
+@end
 
 @implementation PickerViewController
+@synthesize picker;
+@synthesize spinButton;
+@synthesize component1Data;
+@synthesize component2Data;
+@synthesize component3Data;
+@synthesize component4Data;
+@synthesize component5Data;
+@synthesize component6Data;
+@synthesize component1Labels;
+@synthesize component2Labels;
+@synthesize component3Labels;
+@synthesize component4Labels;
+@synthesize component5Labels;
+@synthesize component6Labels;
+@synthesize component1BlurredLabels;
+@synthesize component2BlurredLabels;
+@synthesize component3BlurredLabels;
+@synthesize component4BlurredLabels;
+@synthesize component5BlurredLabels;
+@synthesize component6BlurredLabels;
 
-@synthesize rvController;
-
-- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+- (void)stopBlurring
+{
+	isSpinning = NO;
+}
+- (IBAction)spin
+{	
+	if (! isSpinning)
+	{
+		// Calculate a random index in the array
+		spin1 = arc4random()%[component1Data count];
+		spin2 = arc4random()%[component1Data count];
+		spin3 = arc4random()%[component1Data count];	
+		spin4 = arc4random()%[component1Data count];
+		spin5 = arc4random()%[component1Data count];
+		spin6 = arc4random()%[component1Data count];
+		
+		// Put first and third component near top, second near bottom
+		[picker selectRow:([picker selectedRowInComponent:0]%[component1Data count]) + [component1Data count] inComponent:0 animated:NO];
+		[picker selectRow:(kRowMultiplier - 2) * [component2Data count] + spin2 inComponent:1 animated:NO];
+		[picker selectRow:(kRowMultiplier - 2) * [component3Data count] + spin3 inComponent:2 animated:NO];
+		[picker selectRow:(kRowMultiplier - 2) * [component4Data count] + spin4 inComponent:3 animated:NO];
+		[picker selectRow:(kRowMultiplier - 2) * [component5Data count] + spin5 inComponent:4 animated:NO];
+		[picker selectRow:([picker selectedRowInComponent:2]%[component6Data count]) + [component6Data count] inComponent:5 animated:NO];
+		
+		// Spin to the selected value
+		[picker selectRow:(kRowMultiplier - 2) * [component1Data count] + spin1 inComponent:0 animated:YES];
+		[picker selectRow:spin2 + [component2Data count] inComponent:1 animated:YES];
+		[picker selectRow:spin3 + [component3Data count] inComponent:2 animated:YES];
+		[picker selectRow:spin4 + [component4Data count] inComponent:3 animated:YES];
+		[picker selectRow:spin5 + [component5Data count] inComponent:4 animated:YES];
+		[picker selectRow:(kRowMultiplier - 2) * [component6Data count] + spin6 inComponent:5 animated:YES];
+		
+		isSpinning = YES;
+		
+		// Need to have it stop blurring a fraction of a second before it stops spinning so that the final appearance is not blurred.
+		[self performSelector:@selector(stopBlurring) withObject:nil afterDelay:4.7];
+	}
+}
+- (NSArray *)arrayForComponent:(NSInteger)index
+{
+	NSString *arrayName = [NSString stringWithFormat:@"component%dLabels", index+1];
+	return [self valueForKey:arrayName];
+}
+- (NSArray *)blurredArrayForComponent:(NSInteger)index
+{
+	NSString *arrayName = [NSString stringWithFormat:@"component%dBlurredLabels", index+1];
+	return [self valueForKey:arrayName];
+}
+#pragma mark -
+- (void)_reenablePickerSound
+{
+	[picker setSoundsEnabled:YES];
+}
+- (NSArray *)blurredLabelArrayFromLabelArray:(NSArray *)labelArray
+{
+	NSMutableArray *blurred = [NSMutableArray array];
+	for (UILabel *oneLabel in labelArray)
+	{
+		UIImage *image = [oneLabel getAsImage];
+		UIImage *scaledImage = [image _imageScaledToSize:CGSizeMake(image.size.width / 5.0, image.size.width / 5.0) interpolationQuality:3.0];
+		UIImage *labelImage = [scaledImage _imageScaledToSize:image.size interpolationQuality:3.0];
+		UIImageView *labelView = [[UIImageView alloc] initWithImage:labelImage];
+		[blurred addObject:labelView];
+		[labelView release];
+		
+	}
+	return blurred;
+}
+- (NSArray *)labelArrayFromStringArray:(NSArray *)stringArray
+{
+	NSMutableArray *labels = [NSMutableArray array];
+	for (NSString *oneValue in stringArray)
+	{
+		UILabel *tempLabel =  [[UILabel alloc] initWithFrame:CGRectMake(0.0, 0.0, 30, 15.0)];
+		tempLabel.text = oneValue;
+		tempLabel.font = [UIFont boldSystemFontOfSize:20.0];
+		tempLabel.backgroundColor = [UIColor clearColor];
+		[labels addObject:tempLabel];
+		[tempLabel release];
+	}
+	return labels;
+}
+#pragma mark -
+- (void)viewDidLoad 
+{	
 	
-//[rvController doneSearching_Clicked:nil];
+	UIAccelerometer *accel = [UIAccelerometer sharedAccelerometer];
+	accel.delegate = self;
+	accel.updateInterval = kUpdateInterval;
+	
+	isSpinning = NO;
+	[picker setSoundsEnabled:NO];
+	
+	// Dummy data to display
+	self.component1Data = [NSArray arrayWithObjects:@"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", nil];
+	self.component2Data = [NSArray arrayWithObjects:@"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", nil];
+	self.component3Data = [NSArray arrayWithObjects:@"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", nil];
+	self.component4Data = [NSArray arrayWithObjects:@"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", nil];
+	self.component5Data = [NSArray arrayWithObjects:@"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", nil];
+	self.component6Data = [NSArray arrayWithObjects:@"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", nil];
+	
+	// Create UILabels out of them
+	self.component1Labels = [self labelArrayFromStringArray:component1Data];
+	self.component2Labels = [self labelArrayFromStringArray:component2Data];
+	self.component3Labels = [self labelArrayFromStringArray:component3Data];
+	self.component4Labels = [self labelArrayFromStringArray:component4Data];
+	self.component5Labels = [self labelArrayFromStringArray:component5Data];
+	self.component6Labels = [self labelArrayFromStringArray:component6Data];
+	
+	// Create blurred UIImageViews out of the labels - we don't want to be blurring every time, so we do on load and cache
+	self.component1BlurredLabels = [self blurredLabelArrayFromLabelArray:component1Labels];
+	self.component2BlurredLabels = [self blurredLabelArrayFromLabelArray:component2Labels];
+	self.component3BlurredLabels = [self blurredLabelArrayFromLabelArray:component3Labels];
+	self.component4BlurredLabels = [self blurredLabelArrayFromLabelArray:component4Labels];
+	self.component5BlurredLabels = [self blurredLabelArrayFromLabelArray:component5Labels];
+	self.component6BlurredLabels = [self blurredLabelArrayFromLabelArray:component6Labels];
+	
+	[picker selectRow: [component1Data count] * (kRowMultiplier / 2) inComponent:0 animated:NO];
+	[picker selectRow: [component2Data count] * (kRowMultiplier / 2) inComponent:1 animated:NO];
+	[picker selectRow: [component3Data count] * (kRowMultiplier / 2) inComponent:2 animated:NO];
+	[picker selectRow: [component4Data count] * (kRowMultiplier / 2) inComponent:3 animated:NO];
+	[picker selectRow: [component5Data count] * (kRowMultiplier / 2) inComponent:4 animated:NO];
+	[picker selectRow: [component6Data count] * (kRowMultiplier / 2) inComponent:5 animated:NO];
+	
+	[self performSelector:@selector(_reenablePickerSound) withObject:nil afterDelay:0.25];
+	
+    [super viewDidLoad];
 }
 
-- (void)viewWillAppear:(BOOL)animated {
+- (void)dealloc 
+{
+	[picker release];
+	[spinButton release];
+	[component1Data release];
+	[component2Data release];
+	[component3Data release];
+	[component4Data release];
+	[component5Data release];
+	[component6Data release];
+	[component1Labels release];
+	[component2Labels release];
+	[component3Labels release];
+	[component4Labels release];
+	[component5Labels release];
+	[component6Labels release];
+	[component1BlurredLabels release];
+	[component2BlurredLabels release];
+	[component3BlurredLabels release];
+	[component4BlurredLabels release];
+	[component5BlurredLabels release];
+	[component6BlurredLabels release];
+    [super dealloc];
+}
+#pragma mark -
+#pragma mark UIPickerViewDelegate Methods
+- (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view
+{	
+	int actualRow = row%[[self arrayForComponent:component] count];
 	
-	[super viewWillAppear:YES];
-	arrayNo = [[NSMutableArray alloc] init];
-	[arrayNo addObject:@"1"];
-	[arrayNo addObject:@"2"];
-	[arrayNo addObject:@"3"];
-	[arrayNo addObject:@"4"];
-	[arrayNo addObject:@"5"];
-	[arrayNo addObject:@"6"];
-	[arrayNo addObject:@"7"];
-	[arrayNo addObject:@"8"];
-	[arrayNo addObject:@"9"];
-	code = [[NSMutableArray alloc] init];
-	[code addObject:@"1"];
-	[code addObject:@"1"];
-	[code addObject:@"1"];
-	[code addObject:@"1"];
-	[code addObject:@"1"];
-	[code addObject:@"1"];
-	secret = [NSMutableString stringWithCapacity:6];
+	if (!isSpinning)
+	{
+		NSArray *componentArray = [self arrayForComponent:component];
+		return [componentArray objectAtIndex:actualRow];
+	}
 	
+	NSArray *componentArray = [self blurredArrayForComponent:component];
+	return [componentArray objectAtIndex:actualRow];
+}
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+	NSArray *componentArray = [self arrayForComponent:component];
+	int actualRow = row%[componentArray count];
+	
+	int newRow = ([componentArray count] * (kRowMultiplier / 2)) + actualRow;
+	[picker selectRow:newRow inComponent:component animated:NO];
 	
 }
-
-- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView;
+#pragma mark -
+#pragma mark UIPickerViewDatasource Methods
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
 {
 	return 6;
 }
-
-- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
 {
-	if(row < EXTRA_COMPONENTS) {
-		row += 100;
-		[pickerView selectRow:row inComponent:0 animated:NO];
-		return;
-	} else if(row >= 100 + EXTRA_COMPONENTS) {
-		row -= 100;
-		[pickerView selectRow:row inComponent:0 animated:NO];
-	return;}
-	
-	
-	secret = [[NSMutableString alloc] initWithString:@""];
-	switch (component) {
-		case 0:	
-			
-			[code replaceObjectAtIndex:component withObject:[arrayNo objectAtIndex:row]];
-			break;
-		case 1:
-			
-			[code replaceObjectAtIndex:component withObject:[arrayNo objectAtIndex:row]];
-			
-			break;
-		case 2:
-			
-			[code replaceObjectAtIndex:component withObject:[arrayNo objectAtIndex:row]];			
-			break;
-		case 3:
-			
-			[code replaceObjectAtIndex:component withObject:[arrayNo objectAtIndex:row]];		
-			break;
-		case 4:
-			
-			[code replaceObjectAtIndex:component withObject:[arrayNo objectAtIndex:row]];			
-			break;
-		case 5:
-			
-			[code replaceObjectAtIndex:component withObject:[arrayNo objectAtIndex:row]];		
-			break;
-			
-	}
-	for (int i=0;i<6;i++)
-	{
-		[secret appendString:[code objectAtIndex:i]];
-	}
-//	mlabel6.text = secret;
-	//	if (secret = @"777777")
-	//	{
-	
-	
-	//	}
+	return [[self arrayForComponent:component] count] * kRowMultiplier;
 }
-
-- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component;
-{
-	return 100 + EXTRA_COMPONENTS * 2; // Increase number of components by 10 per side
+#pragma mark -
+#pragma mark UIAccelerometerDelegate Methods
+- (void)accelerometer:(UIAccelerometer *)accelerometer didAccelerate:(UIAcceleration *)acceleration 
+{	
+	if (fabsf(acceleration.x) > kAccelerationThreshold || fabsf(acceleration.y) > kAccelerationThreshold || fabsf(acceleration.z) > kAccelerationThreshold) 
+		[self spin];	
 }
-
-
-- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component;
-{
-	return [arrayNo objectAtIndex:row];
-}
-
-
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
-    // Release anything that's not essential, such as cached data
-}
-
-
-- (void)dealloc {
-	[rvController release];
-    [super dealloc];
-}
-
-
 @end
